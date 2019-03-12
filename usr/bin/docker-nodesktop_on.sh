e5e616bcb56be4f72a6f8fbb858f7d878c4b35c0
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2019-03-12 19:10:45 -0400

    Create docker-nodesktop_on.sh

---
+++ usr/bin/docker-nodesktop_on.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+export CONTAINER="nodesktop"
+export IMAGE="fernandosanchez/nodesktop:0.1"
+export NAME=nodesktop
+export VNC_COL_DEPTH=24
+export VNC_RESOLUTION=1680x1050
+export VNC_PW=mypassword
+export NOVNC_PORT=6901
+export HOME_MOUNT_DIR=/mnt/home
+export ROOT_MOUNT_DIR=/mnt/root
+export RAID_MOUNT_DIR=/mnt/RAID1
+export MY_USERNAME=fersanchez
+
+echo "** Removing previous instances of "$CONTAINER
+
+/usr/bin/docker ps -q --filter "name=$CONTAINER" \
+	| grep -q . \
+	&& /usr/bin/docker stop $CONTAINER \
+	&& /usr/bin/docker rm -fv $CONTAINER
+
+echo "** Starting "$CONTAINER
+
+/usr/bin/docker run \
+--name $CONTAINER \
+-d \
+--restart=always \
+--net=host \
+-p ${NOVNC_PORT}:6901 \
+-v /home/${MY_USERNAME}:${HOME_MOUNT_DIR} \
+-v /mnt/RAID1:${RAID_MOUNT_DIR} \
+-v /etc/group:/etc/group:ro \
+-v /etc/passwd:/etc/passwd:ro \
+-v /etc/shadow:/etc/shadow:ro \
+-v /etc/sudoers.d:/etc/sudoers.d:ro \
+--user 1000:1000 \
+-e VNC_COL_DEPTH=${VNC_COL_DEPTH} \
+-e VNC_RESOLUTION=${VNC_RESOLUTION} \
+-e VNC_PW=${VNC_PW} \
+$IMAGE
+
+echo "** Started "$CONTAINER" from "$IMAGE
