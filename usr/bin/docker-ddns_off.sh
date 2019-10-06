de49fa3a4c182317eafdc1a2c620bb10cdbca7b0
root <fernandosanchezmunoz@gmail.com>
2019-10-06 11:41:10 -0400

    add docker-ddns as an indirect systemd service

---
+++ usr/bin/docker-ddns_off.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export CONTAINER="docker-ddns"
+
+echo "** Stopping "$CONTAINER
+
+/usr/bin/docker ps -q --filter "name=$CONTAINER" \
+	| grep -q . \
+	&& /usr/bin/docker stop $CONTAINER \
+	&& /usr/bin/docker rm -fv $CONTAINER
+
+echo "** Stopped "$CONTAINER
---
+++ usr/bin/docker-ddns_on.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+export CONTAINER="docker-ddns"
+export IMAGE="fernandosanchez/dns-updater"
+
+HOST=ddns.api.hostname.domain.org
+DNS_SECRET=CHANGEME
+PORT=8080
+DOMAIN=$(hostname)
+
+
+echo "** Removing previous instances of "$CONTAINER
+
+/usr/bin/docker ps -q --filter "name=$CONTAINER" \
+	| grep -q . \
+	&& /usr/bin/docker stop $CONTAINER \
+	&& /usr/bin/docker rm -fv $CONTAINER
+
+echo "** Starting "$CONTAINER
+
+/usr/bin/docker run \
+--name $CONTAINER \
+-d \
+--restart=always \
+--net=host \
+-e HOST=$HOST \
+-e SECRET=$DNS_SECRET \
+-e PORT=$PORT \
+-e DOMAIN=$DOMAIN \
+$IMAGE
+
+echo "** Started "$CONTAINER" from "$IMAGE
