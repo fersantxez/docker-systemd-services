45ded88a53a92c24e060ac68fc57024b837eab31
Fernando Sanchez <fersanchez@google.com>
2020-12-13 14:04:19 -0500

    add config files for login info

--- usr/bin/docker-transmission-ovpn_on.sh
+++ usr/bin/docker-transmission-ovpn_on.sh
@@ -4,10 +4,6 @@ export IMAGE="haugene/transmission-openvpn:latest"
 export RUNASUSER="fersanchez"
 export PUID=$(id -u ${RUNASUSER})
 export PGID=$(id -g ${RUNASUSER})
-export OPENVPN_USERNAME="YOURUSERNAME"
-export OPENVPN_PASSWORD="YOURPASSWORD"
-export TRANSMISSION_USERNAME="YOURUSERNAME"
-export TRANSMISSION_PASSWORD="YOURPASSWORD"
 export LOCAL_NETWORK="192.168.0.0/16" #Host network
 export OVPN_WEB_PORT=9091
 export OVPN_TCP_PORT=51413
@@ -19,40 +15,47 @@ export DOWNLOADS_PATH=$(eval echo "~${RUNASUSER}/Downloads")
 #"auth.txt": encrypted username and password from the provider
 #"default.ovpn": configuration from provider including server location to usE
 #https://haugene.github.io/docker-transmission-openvpn/supported-providers/#using_a_custom_provider
-export CREDENTIALS_PATH="/etc/docker-transmission-ovpn/custom"
-export AUTH_TXT=${CREDENTIALS_PATH}"/auth.txt"
-export DEFAULT_OVPN=${CREDENTIALS_PATH}"/default.ovpn"
+export CREDENTIALS_PATH="/etc/docker-transmission-ovpn/custom/"
+export AUTH_TXT=${CREDENTIALS_PATH}"auth.txt"
+export DEFAULT_OVPN=${CREDENTIALS_PATH}"default.ovpn"
+#use a subdir in the user's home folder for credentials (or swap here instead)
+export USERPASS_PATH=$(eval echo "~${RUNASUSER}/.ssh/transmission-ovpn/")
+export OPENVPN_USERNAME=$(cat ${USERPASS_PATH}"ovpn_username")
+export OPENVPN_PASSWORD=$(cat ${USERPASS_PATH}"ovpn_password")
+export TRANSMISSION_USERNAME=$(cat ${USERPASS_PATH}"transmission_username")
+export TRANSMISSION_PASSWORD=$(cat ${USERPASS_PATH}"transmission_password")
 
 if [ -d "$DOWNLOADS_PATH" ]; then
 	echo "** Downloads dir ${DOWNLOADS_PATH} found."
 else
 	echo "** ERROR: Downloads dir ${DOWNLOADS_PATH} not found"
-	echo "** Please review the configuration and ensure it exists"
+	echo "** Please review the configuration and make sure it exists"
 	echo "** Exiting."
 	exit 1
 fi
 
-if [ -f "$AUTH_TXT" ]; then
-	echo "** Auth file ${AUTH_TXT} found."
-else
-	echo "** ERROR: Auth file ${AUTH_TXT} not found"
-	echo "** Please download from https://www.expressvpn.com/setup#manual"
-	echo "** For details check:"
-	echo "https://haugene.github.io/docker-transmission-openvpn/supported-providers/#using_a_custom_provider"
-	echo "** Exiting."
-	exit 1
-fi
+declare -a REQUIRED_FILES=( \
+	${AUTH_TXT} \
+	${DEFAULT_OVPN} \
+	${OPENVPN_USERNAME} \
+	${OPENVPN_PASSWORD} \
+	${TRANSMISSION_USERNAME} \
+	${TRANSMISSION_PASSWORD} \
+)
 
-if [ -f "$DEFAULT_OVPN" ]; then
-	echo "** OpenVPN config file ${DEFAULT_OVPN} found."
-else
-	echo "** ERROR: OpenVPN config file ${DEFAULT_OVPN} not found"
-        echo "** Please download from https://www.expressvpn.com/setup#manual"
-	echo "** For details check:"
-	echo "https://haugene.github.io/docker-transmission-openvpn/supported-providers/#using_a_custom_provider"
-        echo "** Exiting."
-	exit 1
-fi
+for REQUIRED_FILE in "${REQUIRED_FILES[@]}"; do 
+	if [ -f "$REQUIRED_FILE" ]; then
+		echo "** Config file ${REQUIRED_FILE} found."
+	else
+		echo "** ERROR: Config file ${REQUIRED_FILE} not found"
+		echo "** Please check: "
+		echo "https://www.expressvpn.com/setup#manual"
+		echo "https://haugene.github.io/docker-transmission-openvpn/supported-providers/#using_a_custom_provider"
+		echo "** Exiting."
+		exit 1
+	fi
+
+done
 
 echo "** Removing previous instances of "$CONTAINER
 
