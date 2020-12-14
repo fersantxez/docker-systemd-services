3af93bdac6cf537f13b39f0dec5a092489e6520b
Fernando Sanchez <fersanchez@google.com>
2020-12-14 10:48:30 -0500

    remove docker from container name, add share path var

--- usr/bin/docker-plex_on.sh
+++ usr/bin/docker-plex_on.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-export CONTAINER="docker-plex"
+export CONTAINER="plex"
 export IMAGE="plexinc/pms-docker:1.13.9.5456-ecd600442"
 
 MY_PLEX_CLAIM="claim-CHANGEME"
+SHARE_PATH="/share" #Change to the share path
 
 echo "** Removing previous instances of "$CONTAINER
 
@@ -20,11 +21,11 @@ echo "** Starting "$CONTAINER
 --net=host \
 -e TZ=America/New_York \
 -e PLEX_CLAIM=$MY_PLEX_CLAIM \
--v "/share/PLEX/Library/Application Support/Plex Media Server":/config \
+-v ${SHARE_PATH}"/PLEX/Library/Application Support/Plex Media Server":/config \
 -v /home/nobody/plex_temp/transcode:/transcode \
--v /share/Video:/Video \
--v /share/Audio:/Audio \
--v /share/Pictures:/Pictures \
+-v ${SHARE_PATH}/Video:/Video \
+-v ${SHARE_PATH}/Audio:/Audio \
+-v ${SHARE_PATH}/Pictures:/Pictures \
 $IMAGE
 
 echo "** Started "$CONTAINER" from "$IMAGE
