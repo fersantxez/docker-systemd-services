ede85332fa626f24e121a9cdf472df36976df735
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2022-03-15 07:17:43 -0400

    Add docker-ddns.on.sh
    Add as Dynamic DNS server

--- usr/bin/docker-ddns_on.sh
+++ usr/bin/docker-ddns_on.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 export CONTAINER="ddns"
-export IMAGE="fernandosanchez/dns-updater"
+export IMAGE="davd/docker-ddns:1.3.0"
 
-HOST=ddns.api.hostname.domain.org
-DNS_SECRET=CHANGEME
-PORT=8080
-DOMAIN=$(hostname)
-
+ZONE="myexampledomain.net"
+RECORD_TTL=3600
+SHARED_SECRET="changeme"
+#########################
+DNS_TCP_PORT=53
+DNS_UDP_PORT=53
+DDNS_API_PORT=8080
+BIND_MOUNT_PATH="/var/cache/bind" #for persistence across reboots
 
 echo "** Removing previous instances of "$CONTAINER
 
 /usr/bin/docker ps -q --filter "name=$CONTAINER" \
-	| grep -q . \
-	&& /usr/bin/docker stop $CONTAINER \
-	&& /usr/bin/docker rm -fv $CONTAINER
+		| grep -q . \
+		&& /usr/bin/docker stop $CONTAINER \
+		&& /usr/bin/docker rm -fv $CONTAINER
 
 echo "** Starting "$CONTAINER
 
 /usr/bin/docker run \
---name $CONTAINER \
--d \
---restart=always \
---net=host \
--e HOST=$HOST \
--e SECRET=$DNS_SECRET \
--e PORT=$PORT \
--e DOMAIN=$DOMAIN \
-$IMAGE
+	--name $CONTAINER \
+	-d \
+	--restart=always \
+	--net=host \
+        -e ZONE=$ZONE \
+	-e RECORD_TTLE=$RECORD_TTL \
+	-e SHARED_SECRET=$SHARED_SECRET  \
+	$IMAGE
+
 
 echo "** Started "$CONTAINER" from "$IMAGE
